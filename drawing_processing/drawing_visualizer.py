c95931255b24d35fe1690fc834702625ed775e34
EmilianoHFlores <A00833160@tec.mx>
2024-02-18 22:13:25 -0600

    Drawings ready

---
+++ drawing_processing/drawing_visualizer.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+import argparse
+
+# Parse the arguments
+parser = argparse.ArgumentParser()
+# if user passss --timelapse or -t, it will be stored in the variable timelapse as true
+parser.add_argument("--timelapse", "-t", action="store_true")
+args = parser.parse_args()
+
+timelapse = args.timelapse
+
+# Load the points and the width and height
+points = np.load("points.npy")
+print(points)
+WIDTH, HEIGHT = 500, 500
+
+# Create a black image
+img = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+
+# Draw the lines
+for i in range(len(points) - 1):
+    #points are normalized, so we need to multiply by the width and height
+    x1, y1 = points[i] * [WIDTH, HEIGHT]
+    x2, y2 = points[i + 1] * [WIDTH, HEIGHT]
+    cv2.line(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 255), 5)
+    if timelapse:
+        cv2.imshow("Image", img)
+        cv2.waitKey(20)
+
+# Show the image
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ drawing_processing/tkinter_drawer.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+import numpy as np
+
+WIDTH = 1000
+HEIGHT = 1000
+
+class DrawingApp:
+    def __init__(self, root, width=400, height=400):
+        self.root = root
+        self.root.title("Drawing App")
+
+        # Create a canvas
+        self.canvas = tk.Canvas(root, width=width, height=height, bg="white")
+        self.canvas.pack()
+
+        # Initialize an empty list to store points
+        self.points = []
+
+        # Bind mouse events
+        self.canvas.bind("<Button-1>", self.start_drawing)
+        self.canvas.bind("<B1-Motion>", self.draw)
+
+    def start_drawing(self, event):
+        # Record the starting point
+        self.points.append((event.x, event.y))
+
+    def draw(self, event):
+        # Draw a line segment from the last point to the current point
+        x, y = event.x, event.y
+        self.canvas.create_line(self.points[-1][0], self.points[-1][1], x, y, fill="black", width=5)
+
+        # Update the last point
+        self.points.append((x, y))
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DrawingApp(root, width=WIDTH, height=HEIGHT)
+    root.mainloop()
+    print("Done")
+    points = np.array(app.points, dtype=float)
+    points /= [WIDTH, HEIGHT]
+    print(points)
+    np.save("points.npy", points)
+    width_height_file = np.array([WIDTH, HEIGHT])
+    np.save("width_height.npy", width_height_file)
+    
